41cec26369aebe7e592e1b1bc9eef8118c6bca8e
Stella Sun <sunshuyu0413@163.com>
2022-09-11 18:47:26 -0400

    Define the necessary Query and Mutation types

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,45 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    saveBooks: [Book]!
+  }
+
+  type Book {
+    bookId: ID
+    author: String
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Comment {
+    _id: ID
+    commentText: String
+    commentAuthor: String
+    createdAt: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: [User]
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: String!): User
+    removeBook(bookId: ID!): User
+  }
+`;
+
+module.exports = typeDefs;
